b2b5de0fcf653241c3e90ef31dc693211e2caac4
ROMASHSX <andriiromash@gmail.com>
2020-09-02 14:23:45 +0300

    test commit

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
     public static void main(String[] args) throws InvocationTargetException,
             NoSuchMethodException, InstantiationException, IllegalAccessException {
+        //test comment
         ConsoleHandler handler = (ConsoleHandler) Injector.getInstance(ConsoleHandler.class);
         System.out.println("Enter name, surname, age, value and risk for your bet");
         handler.handle();
